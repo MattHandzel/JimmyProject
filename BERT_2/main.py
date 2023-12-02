78ba475aab2157e978919dd3c17ef01df4c9d387
MattHandzel <matthewwalkosz@gmail.com>
2023-12-02 13:15:11 -0600

    finally made it work

--- BERT_2/main.py
+++ BERT_2/main.py
@@ -1,20 +1,76 @@
-import tensorflow as tf
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from transformers import BertTokenizer
 import torch
-import matplotlib.pyplot as plt
 import time
 import pickle
 from transformers import *
 from dataloader import *
-from model import *
 from early_stopper import EarlyStopper
 import pickle
+import argparse
+from model import *
 
 if __name__ == "__main__":
+    hidden_size = 16
+    lr = 1e-4
+    num_hidden_layer = 2
+    num_attention_heads = 2
+    intermediate_size = 16
+    # parse the arguments (which will be the hyperapameters, if there is nothing there, default to the hyperparameters up top)
+    epochs = 100
+    batch_size = 32
+    parser = argparse.ArgumentParser(
+        description="Arguments (hidden_size, lr, num_hidden_layer, num_attention_heads, intermediate_size)\nEx.: python main.py --lr 1e-4 --num_hidden_layer 2 --num_attention_heads 2 --intermediate_size 16 --epochs 100 --batch_size 32"
+    )
+
+    parser.add_argument(
+        "--hidden_size", type=int, default=hidden_size, help="hidden size of the model"
+    )
+    parser.add_argument("--lr", type=float, default=lr, help="learning rate")
+    parser.add_argument(
+        "--num_hidden_layer",
+        type=int,
+        default=num_hidden_layer,
+        help="number of hidden layers",
+    )
+    parser.add_argument(
+        "--num_attention_heads",
+        type=int,
+        default=num_attention_heads,
+        help="number of attention heads",
+    )
+    parser.add_argument(
+        "--intermediate_size",
+        type=int,
+        default=intermediate_size,
+        help="intermediate size",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=epochs,
+        help="number of epochs to train the model",
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=batch_size,
+        help="batch size to train the model",
+    )
+
+    args = parser.parse_args()
+
+    hidden_size = args.hidden_size
+    lr = args.lr
+    num_hidden_layer = args.num_hidden_layer
+    num_attention_heads = args.num_attention_heads
+    intermediate_size = args.intermediate_size
+    epochs = args.epochs
+    batch_size = args.batch_size
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     positives = pd.read_csv("./positive.csv")
     negatives = pd.read_csv("./negative.csv")
@@ -41,10 +97,6 @@ if __name__ == "__main__":
 
     cdr3_sentences, epitope_sentences = construct_sentences(positives)
 
-    shuffle_data(cdr3_sentences, 42)
-    shuffle_data(epitope_sentences, 42)
-    shuffle_data(cdr3_labels, 42)
-
     cdr3_input_ids, cdr3_attention_masks = pad_sentences(
         cdr3_sentences, max_length, tokenizer
     )
@@ -54,6 +106,19 @@ if __name__ == "__main__":
 
     cdr3_combined = torch.cat((cdr3_input_ids, cdr3_attention_masks), dim=1)
     epitope_combined = torch.cat((epitope_input_ids, epitope_attention_masks), dim=1)
+
+    data_amount = 1
+    print("Using ", data_amount * 100, "% of the data")
+
+    shuffle_indexs = np.random.permutation(len(cdr3_labels))
+
+    cdr3_labels = cdr3_labels[shuffle_indexs]
+    cdr3_combined = cdr3_combined[shuffle_indexs]
+    epitope_combined = epitope_combined[shuffle_indexs]
+
+    cdr3_labels = cdr3_labels[: round(len(cdr3_labels) * data_amount)]
+    cdr3_combined = cdr3_combined[: round(len(cdr3_combined) * data_amount)]
+    epitope_combined = epitope_combined[: round(len(epitope_combined) * data_amount)]
 
     cdr3_train_data, cdr3_test_data = train_test_split_no_shuffle(
         cdr3_combined, test_size=0.2
@@ -76,89 +141,44 @@ if __name__ == "__main__":
     epitope_test_data = epitope_test_data.to(device)
 
     # leave only 10% of the labels in the data (just to test it)
-    data_amount = 1
-    print("Using ", data_amount * 100, "% of the data")
-    if data_amount != 1:
-        unique = np.unique(cdr3_labels_train_data)
-        np.random.seed(42)
-        np.random.shuffle(unique)
-        np.sort(unique)
-        the_labels = [
-            np.where(cdr3_labels_train_data == x)[0]
-            for x in unique[: int(len(unique) * 0.1)]
-        ]
-        the_labels = np.concatenate(the_labels)
 
-        cdr3_labels_train_data = cdr3_labels_train_data[the_labels]
-        cdr3_train_data[0] = cdr3_train_data[0][the_labels]
-        cdr3_train_data[1] = cdr3_train_data[1][the_labels]
     loss_func = torch.nn.TripletMarginLoss(
         margin=1.0, p=2.0, eps=1e-06, swap=False, reduction="mean"
     )
 
-    attention_heads = [4, 8, 16]
-    hidden_sizes = [32, 64, 128]
-    num_hidden_layers = [2, 4, 8]
-    intermediate_sizes = [32, 64, 128]
-    i = 0
-    epochs = 1000
-    batch_size = 32
-
-    # flatten configs
-    for hidden_size in hidden_sizes:
-        for num_hidden_layer in num_hidden_layers:
-            for num_attention_heads in attention_heads:
-                for intermediate_size in intermediate_sizes:
-                    config = BertConfig(
-                        hidden_size=hidden_size,
-                        hidden_act="gelu",
-                        initializer_range=0.02,
-                        vocab_size=25,
-                        hidden_dropout_prob=0.1,
-                        num_attention_heads=num_attention_heads,
-                        type_vocab_size=2,
-                        max_position_embeddings=32,
-                        num_hidden_layers=num_hidden_layer,
-                        intermediate_size=intermediate_size,
-                        attention_probs_dropout_prob=0.1,
-                    )
-                    early_stopper = EarlyStopper(
-                        patience=100, min_delta=0, val_data_min_delta=0.001
-                    )
-                    model_save_path = f"./runs/model_hidden_size_{hidden_size}_num_hidden_layer_{num_hidden_layer}_intermediate_size_{intermediate_size}_num_attention_heads_{num_attention_heads}"
-                    model = build_model(config)
-                    # num_params = sum(p.numel() for p in model.parameters())
-                    # print(f"Total Parameters: {num_params}")
-                    #
-                    # print("\nModel's architecture:")
-                    # print(model)
-                    # print("Training the model...")
-                    print(
-                        "Using config: ",
-                        i,
-                        "out of ",
-                        len(attention_heads)
-                        * len(hidden_sizes)
-                        * len(num_hidden_layers)
-                        * len(intermediate_sizes),
-                    )
-                    s_t = time.time()
-                    loss_dict = train_model(
-                        model,
-                        epochs,
-                        batch_size,
-                        cdr3_train_data,
-                        cdr3_labels_train_data,
-                        cdr3_test_data,
-                        cdr3_labels_test_data,
-                        loss_func,
-                        early_stopper,
-                        save_every_x_epochs=10,
-                        model_save_path=model_save_path,
-                    )
-                    print(
-                        "it took ",
-                        round(time.time() - s_t),
-                        "seconds to train the model",
-                    )
-                    i += 1
+    config = BertConfig(
+        hidden_size=hidden_size,
+        hidden_act="gelu",
+        initializer_range=0.02,
+        vocab_size=25,
+        hidden_dropout_prob=0.1,
+        num_attention_heads=num_attention_heads,
+        type_vocab_size=2,
+        max_position_embeddings=32,
+        num_hidden_layers=num_hidden_layer,
+        intermediate_size=intermediate_size,
+        attention_probs_dropout_prob=0.1,
+    )
+    early_stopper = EarlyStopper(patience=100, min_delta=0, val_data_min_delta=0.05)
+    model_save_path = f"./runs/model_hidden_size_{hidden_size}_num_hidden_layer_{num_hidden_layer}_intermediate_size_{intermediate_size}_num_attention_heads_{num_attention_heads}"
+    model = build_model(config)
+    s_t = time.time()
+    loss_dict = train_model(
+        model,
+        epochs,
+        batch_size,
+        cdr3_train_data,
+        cdr3_labels_train_data,
+        cdr3_test_data,
+        cdr3_labels_test_data,
+        loss_func,
+        early_stopper,
+        save_every_x_epochs=1,
+        model_save_path=model_save_path,
+        learning_rate=lr,
+    )
+    print(
+        "it took ",
+        round(time.time() - s_t),
+        "seconds to train the model",
+    )
--- BERT_2/model.py
+++ BERT_2/model.py
@@ -34,41 +34,39 @@ def get_anchor_positive_index_negative_index(labels):
     return random_index_0, random_index_1, negative_index
 
 
-def get_anchors_positives_and_negatives(labels, label_positives_index_map):
+def get_anchors_positives_and_negatives(labels, label_positives_index_map, random=True):
     # For every label, get a positive and negative then return the indexes
     anchors = []
     positives = []
     negatives = []
-    for i, anchor in enumerate(labels):
-        anchors.append(i)
+    random_indicies = np.arange(len(labels))
+    if random:
+        np.random.shuffle(random_indicies)
+
+    for random_index in random_indicies:
+        anchor = labels[random_index]
+        anchors.append(random_index)
+
         positive = np.random.choice(label_positives_index_map[anchor])
         positives.append(positive)
+
         other_keys = list(label_positives_index_map.keys())
         other_keys.remove(anchor)
+
         negative = np.random.choice(
             label_positives_index_map[np.random.choice(other_keys)]
         )
-        # print(
-        #     "anchor label",
-        #     anchor,
-        #     "pos label",
-        #     labels[positive],
-        #     "neg label",
-        #     labels[negative],
-        #     "ai",
-        #     i,
-        #     "pi",
-        #     positive,
-        #     "ni",
-        #     negative,
-        # )
         negatives.append(negative)
+
+    # print("Anchors", anchors)
+    # print("Positives", positives)
+    # print("Negatives", negatives)
 
     return anchors, positives, negatives
 
 
 def train_model(
-    model,
+    model: BertForMaskedLM,
     num_epochs,
     batch_size,
     x_train,
@@ -76,123 +74,133 @@ def train_model(
     x_test,
     x_test_labels,
     loss_func,
-    early_stopper: EarlyStopper = None,
+    early_stopper: EarlyStopper,
     save_every_x_epochs=-1,
     model_save_path="",
+    learning_rate=10e-5,
 ):
-    optimizer = torch.optim.Adam(model.parameters(), lr=10e-5)
-    batch_size = 128
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    keyboard_interrupted = False
+
     train_data_size = x_train.shape[1]
     test_data_size = x_test.shape[1]
-    losses = []
+
+    train_losses = []
     test_losses = []
     label_test_positives_index_map = {}
-    for label in x_test_labels:
-        # Compute labels positive index mpa
-        if label not in label_test_positives_index_map:
-            label_test_positives_index_map[label] = np.where(x_test_labels == label)[0]
     label_positives_index_map = {}
-    for label in x_train_labels:
-        # Compute labels positive index mpa
-        if label not in label_positives_index_map:
-            label_positives_index_map[label] = np.where(x_train_labels == label)[0]
-    for epoch in range(num_epochs):
-        model.train()
 
-        anchor_indexs = []
-        positive_indexs = []
-        negative_indexs = []
-        (
-            anchor_indexs,
-            positive_indexs,
-            negative_indexs,
-        ) = get_anchors_positives_and_negatives(
-            x_train_labels, label_positives_index_map
-        )
-        train_loss = 0
-        for batch in range(0, train_data_size, batch_size):
-            end_batch = batch + batch_size
-            if end_batch > train_data_size:
-                end_batch = train_data_size
-            anchor_output = model(
-                x_train[0][anchor_indexs[batch : batch + batch_size]],
-                x_train[1][anchor_indexs[batch : batch + batch_size]],
+    for label in np.unique(x_test_labels):
+        label_test_positives_index_map[label] = np.where(x_test_labels == label)[0]
+
+    for label in np.unique(x_train_labels):
+        label_positives_index_map[label] = np.where(x_train_labels == label)[0]
+
+    try:
+        for epoch in range(num_epochs):
+            model.train()
+
+            # shuffle data
+
+            anchor_indexs = []
+            positive_indexs = []
+            negative_indexs = []
+
+            (
+                anchor_indexs,
+                positive_indexs,
+                negative_indexs,
+            ) = get_anchors_positives_and_negatives(
+                x_train_labels, label_positives_index_map
             )
-            positive_output = model(
-                x_train[0][positive_indexs[batch : batch + batch_size]],
-                x_train[1][positive_indexs[batch : batch + batch_size]],
+            train_loss = 0
+            for batch in range(0, train_data_size, batch_size):
+                end_batch = batch + batch_size
+                if end_batch > train_data_size:
+                    end_batch = train_data_size
+                anchor_output = model(
+                    x_train[0][anchor_indexs[batch : batch + batch_size]],
+                    x_train[1][anchor_indexs[batch : batch + batch_size]],
+                )
+                positive_output = model(
+                    x_train[0][positive_indexs[batch : batch + batch_size]],
+                    x_train[1][positive_indexs[batch : batch + batch_size]],
+                )
+                negative_output = model(
+                    x_train[0][negative_indexs[batch : batch + batch_size]],
+                    x_train[1][negative_indexs[batch : batch + batch_size]],
+                )
+
+                loss = loss_func(
+                    anchor_output[0], positive_output[0], negative_output[0]
+                )
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                train_loss += loss.detach().numpy() * batch_size
+
+            with torch.no_grad():
+                model.eval()
+
+                (
+                    anchor_indexs,
+                    positive_indexs,
+                    negative_indexs,
+                ) = get_anchors_positives_and_negatives(
+                    x_test_labels, label_test_positives_index_map, random=False
+                )
+                test_loss = 0
+                for batch in range(0, test_data_size, batch_size):
+                    end_batch = batch + batch_size
+                    if end_batch > test_data_size:
+                        end_batch = test_data_size
+
+                    anchor_output = model(
+                        x_test[0][anchor_indexs[batch : batch + batch_size]],
+                        x_test[1][anchor_indexs[batch : batch + batch_size]],
+                    )
+                    positive_output = model(
+                        x_test[0][positive_indexs[batch : batch + batch_size]],
+                        x_test[1][positive_indexs[batch : batch + batch_size]],
+                    )
+                    negative_output = model(
+                        x_test[0][negative_indexs[batch : batch + batch_size]],
+                        x_test[1][negative_indexs[batch : batch + batch_size]],
+                    )
+                    loss = loss_func(
+                        anchor_output[0], positive_output[0], negative_output[0]
+                    )
+                    # print(loss)
+                    test_loss += loss.detach().numpy() * batch_size
+
+            train_loss /= train_data_size
+            train_losses.append(train_loss)
+
+            test_loss /= test_data_size
+            test_losses.append(test_loss)
+            if save_every_x_epochs > 0:
+                if epoch % save_every_x_epochs == 0:
+                    model.save_pretrained(model_save_path + f"/epoch_{epoch}")
+            if early_stopper is not None:
+                if early_stopper.early_stop(test_loss):
+                    break
+            print(
+                f"Epoch: {epoch} | Train loss: {train_loss} | Test loss {test_loss}"  # {summed_test_loss / (eval_every_x_steps / output_every_x_steps)}"
             )
-            negative_output = model(
-                x_train[0][negative_indexs[batch : batch + batch_size]],
-                x_train[1][negative_indexs[batch : batch + batch_size]],
-            )
-
-            loss = loss_func(anchor_output[0], positive_output[0], negative_output[0])
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            train_loss += loss.detach().numpy() * batch_size
-
-        # model evaluation
-        #
-        model.eval()
-
-        (
-            anchor_indexs,
-            positive_indexs,
-            negative_indexs,
-        ) = get_anchors_positives_and_negatives(
-            x_test_labels, label_test_positives_index_map
-        )
-        test_loss = 0
-        for batch in range(0, test_data_size, batch_size):
-            end_batch = batch + batch_size
-            if end_batch > test_data_size:
-                end_batch = test_data_size
-            anchor_output = model(
-                x_test[0][anchor_indexs[batch : batch + batch_size]],
-                x_test[1][anchor_indexs[batch : batch + batch_size]],
-            )
-            positive_output = model(
-                x_test[0][positive_indexs[batch : batch + batch_size]],
-                x_test[1][positive_indexs[batch : batch + batch_size]],
-            )
-            negative_output = model(
-                x_test[0][negative_indexs[batch : batch + batch_size]],
-                x_test[1][negative_indexs[batch : batch + batch_size]],
-            )
-            loss = loss_func(anchor_output[0], positive_output[0], negative_output[0])
-            test_loss += loss.detach().numpy() * batch_size
-
-        train_loss /= train_data_size
-        losses.append(train_loss)
-
-        test_loss /= test_data_size
-        test_losses.append(test_loss)
-        if save_every_x_epochs > 0:
-            if epoch % save_every_x_epochs == 0:
-                # print(model_save_path)
-                # if not os.path.exists(model_save_path):
-                #     os.makedirs(model_save_path)
-                model.save_pretrained(model_save_path + f"/epoch_{epoch}")
-        if early_stopper is not None:
-            print(f"Test loss {test_loss}", early_stopper.min_validation_loss)
-            if early_stopper.early_stop(test_loss):
-                print("Early stopping")
-                break
-        print(
-            f"Epoch: {epoch} | Train loss: {train_loss} | Test loss {test_loss}"  # {summed_test_loss / (eval_every_x_steps / output_every_x_steps)}"
-        )
-    model.save_pretrained(model_save_path + "/final_model")
-
-    plt.plot(losses, label="train")
+        model.save_pretrained(model_save_path + "/final_model")
+    except KeyboardInterrupt:
+        keyboard_interrupted = True
+        print("keyboard interrupted")
+    plt.plot(train_losses, label="train")
     plt.plot(test_losses, label="test")
     plt.legend()
     plt.savefig(model_save_path + "/loss.png")
-    with open(model_save_path + "train_loss.pickle", "wb") as file:
-        pickle.dump(losses, file)
-    with open(model_save_path + "test_loss.pickle", "wb") as file:
+    with open(model_save_path + "/train_loss.pickle", "wb") as file:
+        pickle.dump(train_losses, file)
+    with open(model_save_path + "/test_loss.pickle", "wb") as file:
         pickle.dump(test_losses, file)
 
     print("Array has been successfully saved using pickle.")
-    return {"train_loss": losses, "test_loss": test_losses}
+    if keyboard_interrupted:
+        raise KeyboardInterrupt("You interrupted the keyboard!")
+    return {"train_loss": train_loss, "test_loss": test_losses}
